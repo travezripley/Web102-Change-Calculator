872fea1cfa0b719f7522a47c62d65a7c612713b9
Travezripley <theripleys@gmail.com>
2019-03-04 13:38:41 -0800

    adds initial project files

--- main.js
+++ main.js
@@ -1,1 +1,63 @@
-// Write your JavaScript here
+
+//Web102-Change-Calculator - main.js  
+//Created by Travis Ripley
+//Started February 25th, 2019.
+
+
+//Monetary Values
+let change = {
+    totalChange: 0,
+    /*   dollar: {
+          one: 0,
+          five: 0,
+          ten: 0,
+          twenty: 0
+      }, */
+    quarters: 0,
+    dimes: 0,
+    nickels: 0,
+    pennies: 0,
+};
+
+function calculateChange() {
+    //verif it button was clicked
+    console.log("button was clicked")
+
+    //user inputs
+
+    //Amount Due
+    let amountDue = document.getElementById('amount-due').value * 100;
+
+    //Amount Received
+    let amountReceived = document.getElementById("amount-received").value * 100;
+
+    //calculation from the inputs
+    let subTotal = (amountReceived) - (amountDue);
+
+    /* let totalResult = document.getElementById("amount-received").value - 
+    document.getElementById('amount-due').value;
+    document.getElementById('total').innerHTML = totalChange; */
+
+    //logging inputs to the console (for verification)
+    console.log(amountDue, amountReceived, subTotal);
+
+   //change in *Dollars
+    let change$ = Math.floor((subTotal)/100);
+    document.getElementById('dollars-output').innerHTML = change$;
+
+    //change in *Quarters
+    let changeQ = Math.floor(((subTotal)-(change$*100))/25);
+    document.getElementById('quarters-output').innerHTML = changeQ;
+
+    //change in *dimes
+    let changeD = Math.floor(((subTotal)-(change$*100)-(changeQ*25))/10);
+    document.getElementById('dimes-output').innerHTML = changeD;
+
+    //change in *nickles
+    let changeN = Math.floor(((subTotal)-(change$*100)-(changeQ*25)-(changeD*10))/5);
+    document.getElementById('nickels-output').innerHTML = changeN;
+
+    //change in *pennies 
+    let changeP = Math.ceil(((subTotal)-(change$*100)-(changeQ*25)-(changeD*10)-(changeN*5))/1);
+    document.getElementById('pennies-output').innerHTML = changeP;
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+March 4th, 2019
+
+Hey guys, thanks for Checking out my code in my Github.
+
+What you see here is a "Change Calculator" webapp. This is what happens, Upon loading the app, the interface has two inputs "Amount Due" and "Amount Received". This is where the client inputs the price of the item(s) requested from the customer and the amount of currency in "USA currency" the client recieves from the customer. Pressing the "Calculate Change" button will display the change broken down in "Dollars, Quarters, Dimes, Nickles and Penny's returned to the customer.
+
+The code was written in HTML, CSS and Javascript.
+
+To get started, open the app in VS code, or code editor of your choosing, and install the dependencies via the terminal by typing '$ npm install'. Next, in the terminal, type '$ static .' to start the app on the local node server and open the localhost:8080 in the browser of your choice.
+
+Please Comment, Like and Subscribe to my Youtube Channel. Tell your Friends, Your Programming Pal, -Travez
+
+Theripleys@gmail.com https://www.youtube.com/c/TravisRipley
